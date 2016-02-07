1b297e5c1880cc51506f2d1c6c07d2d265edfe3e
shtain <goldshtain@inbox.lv>
2016-02-07 16:36:01 +0200

    darits

--- implode.php
+++ implode.php
@@ -1,8 +1,2 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$name = array('matīss','Caks Noriss', 'Maikls');
