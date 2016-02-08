c3881b920084860387bcf0bcb3640bb41e6c6634
shtain <goldshtain@inbox.lv>
2016-02-08 10:27:32 +0200

    izlabots kkas

--- functions.php
+++ functions.php
@@ -12,9 +12,14 @@ function divuCiparuSumma($num1, $num2){
 }
 echo divuCiparuSumma('1','3');
 echo "<hr>";
-function resetNumbersToZerro($var){  // & -> Nozīmē ka $x vērtība tiek piešķirta pēc funkcijas izpildes (Ja godīgi tad neiepraucu īsti)
+function resetNumbersToZerro($var){  // & -> Nozīmē ka $x vērtība tiek piešķirta pēc funkcijas izpildes, cik saprotu tiek izmainīta vērtība pasam $x nevis pēc tam (Ja godīgi tad neiepraucu īsti)
     $var = 0;
     }
     $x = 900;
     resetNumbersToZerro($x);
     echo $x;
+echo "<hr>";
+$gads = date('Y') + 4; 
+$menesis = date('F') ;
+$dienas = cal_days_in_month(CAL_GREGORIAN, $menesis, $gads);
+echo $gads.' gada '. $menesis .' ir '. $dienas .' dienas';
